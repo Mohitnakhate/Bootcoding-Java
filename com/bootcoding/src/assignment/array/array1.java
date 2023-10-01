68bc15049f4f53a7edfffcfcf2360efb77b72365
Mohitnakhate <mpnakhate@gmail.com>
2023-10-01 22:29:43 +0530

    array assignment

---
+++ com/bootcoding/src/assignment/array/array1.java
@@ -0,0 +1,10 @@
+package com.bootcoding.src.assignment.array;
+
+public class array1 {
+    public static void main(String[]args){
+        int arr[] = {10,15,20,25,30};
+        for (int i=0;i<5;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
---
+++ com/bootcoding/src/assignment/array/array2.java
@@ -0,0 +1,10 @@
+package com.bootcoding.src.assignment.array;
+
+public class array2 {
+    public static void main(String[]args){
+        String f[] = {"Apple","Avocado","Grapes","Pineapple","Watermelon"};
+        for (int i=0;i<5;i++){
+            System.out.print(f[i]+" ");
+        }
+    }
+}
